ce7dd1e342a7edaed244cf9a062c09e2edb9fd54
Carl Worth <cworth@cworth.org>
2016-08-31 11:18:33 -0700

    Rename rtmbot to stony
    For both the top-level command name (stony.py) as well as the
    configuration file (stony.conf).

--- stony.py
+++ stony.py
@@ -18,7 +18,7 @@ def parse_args():
 
 # load args with config path
 args = parse_args()
-config = yaml.load(open(args.config or 'rtmbot.conf', 'r'))
+config = yaml.load(open(args.config or 'stony.conf', 'r'))
 bot = RtmBot(config)
 try:
     bot.start()
