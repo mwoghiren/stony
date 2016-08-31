81cd919f4f85f2d26d89006234d608aebe8a8047
Carl Worth <crwth@puzzlers.org>
2016-08-31 12:10:07 -0700

    Switch to a ClueState class instead of just a dictionary
    This makes the syntax a little easier to read, (such as "st.count"
    instead of "st['count']").

--- plugins/clue/clue.py
+++ plugins/clue/clue.py
@@ -7,21 +7,27 @@ outputs = []
 state = {}
 
 
+class ClueState:
+
+    def __init__(self):
+        self.count = 0
+        self.clue = ''
+
 def process_message(data):
 
     channel = data['channel']
     if channel not in state.keys():
-        state[channel] = {'count': 0, 'clue': ''}
+        state[channel] = ClueState()
 
     st = state[channel]
 
     # Count the number of messages we have seen in this channel since
     # stony last repeated a clue.
-    st['count'] = st['count'] + 1
+    st.count = st.count + 1
 
     if data['text'].startswith('&gt;'):
-        st['clue'] = data['text']
-        st['count'] = 1
+        st.clue = data['text']
+        st.count = 1
     else:
-        if st['count'] % 10 == 0:
-            outputs.append([channel, st['clue']])
+        if st.count % 10 == 0:
+            outputs.append([channel, st.clue])
