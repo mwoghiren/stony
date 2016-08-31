05b7ee7b58d023e25c738d29aded477b7654956c
Carl Worth <crwth@puzzlers.org>
2016-08-31 12:02:51 -0700

    Teach the clue plugin to remember and remind clues periodically
    The clue plugin will now keep track of data on a per-channel basis. It
    will remember the clue for each channel (a message beginning with ">")
    and will repeat it every time there have been 10 intervening non-clue
    posts.

--- plugins/clue/clue.py
+++ plugins/clue/clue.py
@@ -4,8 +4,24 @@ from __future__ import unicode_literals
 crontable = []
 outputs = []
 
+state = {}
+
 
 def process_message(data):
-    if data['type'] == 'message':
-        if data['text'].startswith('&gt;'):
-            outputs.append([data['channel'], data['text']])
+
+    channel = data['channel']
+    if channel not in state.keys():
+        state[channel] = {'count': 0, 'clue': ''}
+
+    st = state[channel]
+
+    # Count the number of messages we have seen in this channel since
+    # stony last repeated a clue.
+    st['count'] = st['count'] + 1
+
+    if data['text'].startswith('&gt;'):
+        st['clue'] = data['text']
+        st['count'] = 1
+    else:
+        if st['count'] % 10 == 0:
+            outputs.append([channel, st['clue']])
