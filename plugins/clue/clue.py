012bd4e09c7ed08ff79ce039a9682a5249fef1fc
Carl Worth <crwth@puzzlers.org>
2016-08-31 11:28:24 -0700

    Allow stony to talk in channels other than direct-message channels.
    A direct-message channel identifier always starts with the character
    'D', (while other channels have identifiers that start with 'C'). By
    lifting the restriction of the channel name starting with "D", stony
    can now talk in any channel.
    
    Note: There's nothing we can do in the code to instruct stony to join
    any particular channel. Instead, stony will need to be invited to any
    channel of interest.

--- plugins/clue/clue.py
+++ plugins/clue/clue.py
@@ -6,7 +6,6 @@ outputs = []
 
 
 def process_message(data):
-    if data['channel'].startswith("D"):
-        outputs.append([data['channel'], "from repeat1 \"{}\" in channel {}".format(
-            data['text'], data['channel'])]
-        )
+    outputs.append([data['channel'], "from repeat1 \"{}\" in channel {}".format(
+        data['text'], data['channel'])]
+    )
